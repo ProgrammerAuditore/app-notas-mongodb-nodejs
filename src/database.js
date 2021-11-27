ea07ec7ec778212fdd871df83d16e37ee44a8cbf
ProgrammerAuditore <victorvj098@gmail.com>
2021-11-27 17:26:20 -0600

    *** Hacer la conexion de la base de datos de MongoDB #2

--- src/database.js
+++ src/database.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+
+// Establecer la conexión de base de datos MongoDB usando moongose
 mongoose.connect('mongodb://localhost/notes-db-app')
     .then( db => console.log('Database Connected...'))
     .catch( err => console.log(err));
