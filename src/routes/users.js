31beb35c0b676f0b15cae34bf1874bab4b295d96
ProgrammerAuditore <victorvj098@gmail.com>
2021-11-28 15:06:59 -0600

    *** Se crea la ruta para registrar usuario y la vista

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
 
+// Ruta para mostrar la vista de iniciar sesión
 router.get('/users/singin', (req, res) => {
     res.render('users/singin');
 });
 
+// Ruta para mostrar la vista de registrarme
 router.get('/users/singup', (req, res) => {
     res.render('users/singup');
+});
+
+// Ruta para registrar los datos de un usuario
+router.post('/users/singup', (req, res) => {
+    const { name, email, password, checkpassword } = req.body;
+    const errors = [];
+
+    if( password != checkpassword){
+        errors.push({ text: 'La contraseña no coinciden'});
+    }
+
+    if( password.length > 4 ){
+        errors.push({ text : 'La contraseña debe ser mayor a 4 caracteres'});
+    }
+
+    if( errors.length > 0  ){
+        res.render('users/singup', { errors, name, email, password, checkpassword });
+    }else{
+        res.send(`${name} - ${email} - ${password} - ${checkpassword} `);
+    }
 });
 
 module.exports = router;
