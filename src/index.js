6a08edce7e4a4a847466a402a441436bc362dfe0
ProgrammerAuditore <victorvj098@gmail.com>
2021-11-27 22:35:55 -0600

    *** Se agrego la configuracion de flash

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
 
 // * Solventar el error:
 // * https://handlebarsjs.com/api-reference/runtime-options.html#options-to-control-prototype-access
@@ -47,8 +48,17 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+// Para mostrar mensajes
+app.use(flash());
 
 // ! Global variables
+// Mostar mensajes flash en todas las vistas
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+
+    next();
+});
 
 // ! Routes
 app.use(require('./routes/index'));
