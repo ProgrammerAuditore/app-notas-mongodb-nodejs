577b9a711e0161534fdb41cb02d3fb50cd1671ea
ProgrammerAuditore <victorvj098@gmail.com>
2021-11-27 18:25:01 -0600

    *** Se creo un ruta, la vista y su contenido para crear una nota

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+// Ruta para crear una nota
+router.get('/notes/add', (req, res) => {
+    res.render('notes/nueva-nota');
+});
+
+// Ruta para ver notas de un usuario
 router.get('/notes', (req, res) => {
     res.send('Mis notas');
 });
