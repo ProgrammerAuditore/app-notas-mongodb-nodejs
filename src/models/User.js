9ef8b5297b1af85bd43ca44a00fa2b3b5fe4b702
ProgrammerAuditore <victorvj098@gmail.com>
2021-11-28 15:45:25 -0600

    *** Se creo metodos para el Schema de usuarios en del modelo de usuarios

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
 
 // Crear un modelo 
@@ -8,5 +9,17 @@ const UserSchema = new Schema({
     password :  { type: 'String', required: true }, 
     date : { type: Date, default: Date.now() }
 });
+
+// Método de el schema para encriptar contraseñas
+UserSchema.methods.encrypPassword = async (password) => {
+    const salt = await bcrypt.genSalt(13);
+    const hash = bcrypt.hash(password, salt);
+    return hash;
+};
+
+// Método de el schema para comparar contraseñas
+UserSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
